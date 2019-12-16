aad959d7b2027ea0d3899f02290e4431120c33ff
panda-star <deanreagon13@gmail.com>
2019-12-16 22:22:58 +0200

    Create app.js

---
+++ app.js
@@ -0,0 +1,17 @@
+var http = require("http");
+var fs = require("fs");
+
+var server = http.createServer(function(req, res){
+    console.log("request was made: " + req.url);
+    if (req.url === "/home"){
+        res.writeHead( 200, {'Content-Type': 'text/html'});
+        fs.createReadStream(__dirname + "/index.html").pipe(res);   
+    }
+    else{
+        res.writeHead( 200, {'Content-Type': 'text/plain'});
+        res.end("hello world");
+    }
+});
+
+server.listen(3000, '127.0.0.1');
+console.log("listening to port 3000");
