6b136f5360d9c8725c03f380d5e235f1f86c8aff
panda-star <deanreagon13@gmail.com>
2019-12-20 16:47:24 +0200

    added property schema

---
+++ models/property.js
@@ -0,0 +1,59 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// create Schema & model
+const PropertySchema = new Schema({
+   title: {
+       type: String,
+       required: [true, "name is required"]
+   },
+
+//    image: {
+//        type: String
+//    },
+
+    // estate agent who handles any queries for the house
+    agent: {
+        // // type might cause issues
+        // type: user,
+        
+        // this is the id of the estate agent from the user collection
+        type: String,
+        required: [true, "agent is required"]
+    },  
+    
+    // date first posted
+    date: {
+        type: Date,
+        required: [true, "date is required"]
+    },  
+
+    // short description of the house
+    description: {
+        type: String,
+        required: [true, "description is required"]
+    },  
+
+    price: {
+        type: Number,
+        required: [true, "price is required"]
+    },  
+
+    // is the house for sale or not
+    avaliable: {
+       type: Boolean,
+       default: true
+    },
+
+    address: {
+        type: String,
+        required: [true, "address is required"]
+    }
+
+});
+
+// add in geo location
+
+const Property = mongoose.model("property", PropertySchema);
+
+module.exports = Property;
